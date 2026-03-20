20f26faafa8457175e00808fe7b7bec37fb5eff6
Salsa-Lat <salsa.lat71@gmail.com>
2026-03-20 14:01:06 +0100

    s

---
+++ back/routes/links.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// read links.json
+function readJSON(filePath) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const json = JSON.parse(data);
+
+  return json.sort((a, b) => a.order - b.order);
+}
+const link = readJSON('../data/links.json');
+
+
+app.get('/api/links', (req, res) => {
+  res.json(link);
+});
